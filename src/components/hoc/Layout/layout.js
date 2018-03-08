a755321b62201cd80bd286bfe8979f63ddd1b207
William Elliott <wcelliott1991@gmail.com>
2018-03-08 13:50:43 -0500

    creates footer component and configures a helper variable to fetch the current year

--- src/components/hoc/Layout/layout.js
+++ src/components/hoc/Layout/layout.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './layout.css';
 
 import Header from '../../Header/header';
+import Footer from '../../Header/Footer/footer';
 
 class Layout extends Component {
     state = {
@@ -23,7 +24,7 @@ class Layout extends Component {
                     onOpenNav={() => this.toggleSideNav(true)}
                 />
                 {this.props.children}
-                Footer
+                <Footer />
             </div>
         )
     }
